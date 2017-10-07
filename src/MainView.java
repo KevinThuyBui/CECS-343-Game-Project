a7e520a5e0312bd2a407d31275dc27a9ed14a5ff
Kevin Bui <sinat123@hotmail.com>
2017-10-06 19:06:18 -0700

    Added image to scrollPane

--- src/MainView.java
+++ src/MainView.java
@@ -1,10 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import java.io.IOException;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import java.awt.image.BufferedImage;
 
 public class MainView extends JFrame {
 
@@ -34,16 +40,34 @@ public class MainView extends JFrame {
 
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100,100, 1500, 1500);
+		setBounds(100,100, 1200, 1000);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		//Create a scrollPane for map
-		JScrollPane scrollPane = new JScrollPane();
-		getContentPane().add(scrollPane, BorderLayout.NORTH);
+		ImageIcon CSULBMapIcon = null;
+
+		//Create the image for the scrollPane
+		BufferedImage CSULBMapImage = null;
 		
+		try{
+			CSULBMapImage = ImageIO.read(new File ("c:\\Users\\Milksauce\\CECS-343-Game-Project\\resources\\CSULBMap5_1200x1437.png"));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+
+		CSULBMapIcon = new ImageIcon(CSULBMapImage);
+		javax.swing.JLabel CSULBMapLabel = new javax.swing.JLabel(CSULBMapIcon);
+
+		
+		JScrollPane scrollPane = new JScrollPane(CSULBMapLabel);
+		scrollPane.setSize(1000, 1000);
+		getContentPane().add(scrollPane, BorderLayout.CENTER);	
+	
 		//Create a panel to hold the player information and interactions below
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.SOUTH);
