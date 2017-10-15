29835551905941e168fb8628acc4e58328ad6b8d
Kevin Bui <sinat123@hotmail.com>
2017-10-14 22:07:19 -0700

    Refactoed the ScrollPane to MapScrollPane

--- src/Room.java
+++ src/Room.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-enum Room{
+enum Rooms {
 
     GEORGE_ALLEN_FIELD(10, 80, "Japanese Garden, Pyramid, Rec Center, West Walkway"),
     JAPANESE_GARDEN(310, 20, "George Allen Field, Pyramid, Student Parking"),
@@ -13,45 +13,53 @@ enum Room{
     FORBIDDEN_PARKING(715, 420, "Student Parking, East Walkway, Rec Center"),
     WEST_WALKWAY(10, 470, "George Allen Field, Rec Center, North Hall, Library"),
     LIBRARY(10, 1170, "West Walkway, LA 5"),
-    LA5(355, 1190, "Library, Elevators, Bratwurst Hall"),
+    LA_5(355, 1190, "Library, Elevators, Bratwurst Hall"),
     BRATWURST_HALL(760, 1190, "LA 5, East Walkway"),
     EAST_WALKWAY(1045, 700, "Forbidden Parking, South Hall, Bratwurst Hall"),
     COMPUTER_LAB(130, 640, "North Hall"),
-    ECS302(440, 630, "North Hall, South Hall"),
+    ECS_302(440, 630, "North Hall, South Hall"),
     EAT_CLUB(740, 630, "South Hall"),
     CECS_CONFRENCE_ROOM(900, 640, "South Hall"),
     NORTH_HALL(125, 825, "West Walkway, Computer Lab, ECS 302, Elevators, Room of Retirement"),
     SOUTH_HALL(595, 825, "ECS 302, EAT Club, CECS Conference Room, Lactation Lounge, ECS 308, North Hall"),
-    ROOM_OF_RETIREMENT(130, 970,"North Hall"),
+    ROOM_OF_RETIREMENT(130, 970, "North Hall"),
     ELEVATORS(430, 970, "North Hall, LA 5"),
-    ECS308(595, 970, "South Hall"),
+    ECS_308(595, 970, "South Hall"),
     LACTATION_LOUNGE(880, 960, "South Hall");
 
-    private final String roomAdjacency;
-    private final int xDrawPosition;
-    private final int yDrawPosition;
+    public final String roomAdjacency;
+    public final int xDrawPosition;
+    public final int yDrawPosition;
 
-    Room(int xDrawPosition, int yDrawPosition, String roomAdjacency)
-    {
+    Rooms(int xDrawPosition, int yDrawPosition, String roomAdjacency) {
         this.roomAdjacency = roomAdjacency;
         this.xDrawPosition = xDrawPosition;
         this.yDrawPosition = yDrawPosition;
     }
 
+}
+public class Room
+{
+    private Rooms currentRoom;
+    public Room(){
+        currentRoom = Rooms.ECS_308;
+    }
+    //Returns the Point at which the Player token should be dreawn
     public Point getDrawPosition()
     {
-        return new Point(xDrawPosition, yDrawPosition);
+        return new Point(currentRoom.xDrawPosition, currentRoom.yDrawPosition);
     }
 
-    public List<String> getAdjacentRoomStrings()
+    //Returns a List of Strings of the name of the adjacent rooms
+    List<String> getAdjacentRoomStrings()
     {
-        return processString(roomAdjacency);
+        return processString(currentRoom.roomAdjacency);
     }
 
-
-    public Room getEnum(String inputString)
+    //When given the string chosen from List<String> from getAdjacentRoomStrings returns the Enum type for processing
+    void setRoom(String inputString)
     {
-        return Room.valueOf(processEnumString(inputString));
+        currentRoom = Rooms.valueOf(processEnumString(inputString));
     }
 
     private String processEnumString(String inputString)
